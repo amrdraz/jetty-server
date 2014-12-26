bdd13976889e56beef5190724ec6f0949cf52da6
Amr Draz <amr.m.draz@gmail.com>
2014-12-26 19:10:21 +0200

    started using servlet

--- HelloWorld.java
+++ HelloWorld.java
@@ -50,8 +50,15 @@ public class HelloWorld extends AbstractHandler
 
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(3678);
-        server.setHandler(new HelloWorld());
+        int port = 3678;
+        if (args.length>0) {
+            port = Integer.parseInt(args[0]);
+        }
+        Server server = new Server(port);
+        
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
  
         server.start();
         server.join();
